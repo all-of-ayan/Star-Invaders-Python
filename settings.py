9aab3e17513090eb06c37bdcf6493f60b7239345
all-of-ayan <thataayan@gmail.com>
2026-06-30 01:37:44 +0500

    Completed scoreboard functionality added format specifier

--- settings.py
+++ settings.py
@@ -9,12 +9,12 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 3
+        self.ship_speed = 5
         self.ship_limit = 3
 
         # Bullet settings
         self.bullet_speed = 2.5
-        self.bullet_width = 3
+        self.bullet_width = 30
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 4
@@ -27,7 +27,8 @@ class Settings:
 
         # How quickly the game speeds up. 
         self.speedup_scale = 1.25
-
+        self.score_scale = 1.5
+        
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -43,7 +44,9 @@ class Settings:
         self.fleet_direction = 1
     
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self. alien_points = int(self.alien_points * self.score_scale)
